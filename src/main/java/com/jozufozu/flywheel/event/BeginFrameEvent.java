931b4a33aefcce743baffad3b2feb10c5b8b589e
Jozufozu <jozsefaug@gmail.com>
2021-08-05 14:53:22 -0700

    Event adjustments to work with optifine shadows

--- src/main/java/com/jozufozu/flywheel/event/BeginFrameEvent.java
+++ src/main/java/com/jozufozu/flywheel/event/BeginFrameEvent.java
@@ -7,22 +7,17 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.culling.ClippingHelper;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.eventbus.api.Event;
 
 public class BeginFrameEvent extends Event {
 	private final ClientWorld world;
-	private final MatrixStack stack;
 	private final ActiveRenderInfo info;
-	private final GameRenderer gameRenderer;
-	private final LightTexture lightTexture;
 	private final ClippingHelper clippingHelper;
 
-	public BeginFrameEvent(ClientWorld world, MatrixStack stack, ActiveRenderInfo info, GameRenderer gameRenderer, LightTexture lightTexture, ClippingHelper clippingHelper) {
+	public BeginFrameEvent(ClientWorld world, ActiveRenderInfo info, ClippingHelper clippingHelper) {
 		this.world = world;
-		this.stack = stack;
 		this.info = info;
-		this.gameRenderer = gameRenderer;
-		this.lightTexture = lightTexture;
 		this.clippingHelper = clippingHelper;
 	}
 
@@ -30,23 +25,15 @@ public class BeginFrameEvent extends Event {
 		return world;
 	}
 
-	public MatrixStack getStack() {
-		return stack;
-	}
-
 	public ActiveRenderInfo getInfo() {
 		return info;
 	}
 
-	public GameRenderer getGameRenderer() {
-		return gameRenderer;
-	}
-
-	public LightTexture getLightTexture() {
-		return lightTexture;
-	}
-
 	public ClippingHelper getClippingHelper() {
 		return clippingHelper;
+	}
+
+	public Vector3d getCameraPos() {
+		return info.getPosition();
 	}
 }
