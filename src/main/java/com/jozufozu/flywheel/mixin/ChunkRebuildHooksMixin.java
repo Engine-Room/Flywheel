39ce96ec9c0a5e3c1748ca913a235afdebb08b1d
Jozufozu <jozsefaug@gmail.com>
2021-09-26 22:17:14 -0700

    Better Block Entity filtering
     - Assumes IInstanceRendered#shouldRenderNormally does not change over a BE's life

---
+++ src/main/java/com/jozufozu/flywheel/mixin/ChunkRebuildHooksMixin.java
@@ -0,0 +1,30 @@
+package com.jozufozu.flywheel.mixin;
+
+import java.util.Set;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.jozufozu.flywheel.backend.Backend;
+import com.jozufozu.flywheel.backend.instancing.InstancedRenderRegistry;
+
+import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+@Mixin(targets = "net.minecraft.client.renderer.chunk.ChunkRenderDispatcher$RenderChunk$RebuildTask")
+public class ChunkRebuildHooksMixin {
+
+	@Inject(method = "handleBlockEntity", at = @At("HEAD"), cancellable = true)
+	private <E extends BlockEntity> void filterBlockEntity(ChunkRenderDispatcher.CompiledChunk compiledChunk, Set<BlockEntity> set, E be, CallbackInfo ci) {
+
+		if (Backend.getInstance().canUseInstancing() && Backend.isFlywheelWorld(be.getLevel()) && InstancedRenderRegistry.getInstance()
+				.shouldSkipRender(be)) {
+			ci.cancel();
+		}
+	}
+}
