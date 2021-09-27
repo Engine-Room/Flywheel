79ac9e96e4d6f4239a581709a4ab60c69554c23e
Jozufozu <jozsefaug@gmail.com>
2021-09-26 21:15:02 -0700

    Testing + fixing

--- src/main/java/com/jozufozu/flywheel/backend/gl/attrib/IAttribSpec.java
+++ src/main/java/com/jozufozu/flywheel/backend/gl/attrib/IAttribSpec.java
@@ -2,7 +2,7 @@ package com.jozufozu.flywheel.backend.gl.attrib;
 
 public interface IAttribSpec {
 
-	void vertexAttribPointer(int stride, int index, int pointer);
+	void vertexAttribPointer(int stride, int index, int offset);
 
 	int getSize();
 
