4a86c112ddd54e7e469d33ca223177f0cf9d3c68
Jozufozu <jozsefaug@gmail.com>
2021-12-11 17:31:09 -0800

    Cylindrical fog
     - having the world position is very nice
     - mojang manages to do multiple matrix multiplications

--- src/main/resources/assets/flywheel/flywheel/shaders/context/fog.glsl
+++ src/main/resources/assets/flywheel/flywheel/shaders/context/fog.glsl
@@ -1,5 +1,3 @@
-#define USE_FOG
-
 #if defined(VERTEX_SHADER)
 out float FragDistance;
 #elif defined(FRAGMENT_SHADER)
@@ -7,6 +5,18 @@ in float FragDistance;
 #endif
 uniform vec4 uFogColor;
 uniform vec2 uFogRange;
+
+float cylindrical_distance(vec3 worldPos, vec3 cameraPos) {
+    float distXZ = length(worldPos.xz - cameraPos.xz);
+    float distY = abs(worldPos.y - cameraPos.y);
+    return max(distXZ, distY);
+}
+
+float cylindrical_distance(vec3 worldPos) {
+    float distXZ = length(worldPos.xz);
+    float distY = abs(worldPos.y);
+    return max(distXZ, distY);
+}
 
 float FLWFogFactor() {
     return (uFogRange.y - FragDistance) / (uFogRange.y - uFogRange.x);
