c15399512104ff1a798022eae80b305b994ab861
Jozufozu <jozsefaug@gmail.com>
2021-12-25 01:01:47 -0800

    No more diffuse divide
     - Move PlacementSimulationWorld to Flywheel as VirtualRenderWorld
     - Simplify ModelTransformer
     - Model doesn't need #configure

--- src/main/java/com/jozufozu/flywheel/core/model/WorldModel.java
+++ src/main/java/com/jozufozu/flywheel/core/model/WorldModel.java
@@ -2,9 +2,9 @@ package com.jozufozu.flywheel.core.model;
 
 import java.util.Collection;
 
-import com.jozufozu.flywheel.core.Formats;
 import com.jozufozu.flywheel.api.vertex.VertexList;
 import com.jozufozu.flywheel.api.vertex.VertexType;
+import com.jozufozu.flywheel.core.Formats;
 
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -15,6 +15,9 @@ public class WorldModel implements Model {
 	private final VertexList reader;
 	private final String name;
 
+	/**
+	 * It is expected that <code>renderWorld.getShade(...)</code> returns a constant.
+	 */
 	public WorldModel(BlockAndTintGetter renderWorld, RenderType layer, Collection<StructureTemplate.StructureBlockInfo> blocks, String name) {
 		reader = Formats.BLOCK.createReader(ModelUtil.getBufferBuilderFromTemplate(renderWorld, layer, blocks));
 		this.name = name;
