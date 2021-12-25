e16b32e1166b24021301c111fc4145a7aa950f4b
Jozufozu <jozsefaug@gmail.com>
2021-12-24 16:15:20 -0800

    Track VBO and EBO via vanilla

--- src/main/java/com/jozufozu/flywheel/mixin/BufferUploaderAccessor.java
+++ src/main/java/com/jozufozu/flywheel/mixin/BufferUploaderAccessor.java
@@ -5,12 +5,20 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 import com.mojang.blaze3d.vertex.BufferUploader;
 
-import net.minecraft.client.renderer.ShaderInstance;
-
 @Mixin(BufferUploader.class)
 public interface BufferUploaderAccessor {
 	@Accessor("lastVertexArrayObject")
 	static void flywheel$setLastVAO(int id) {
+		throw new AssertionError();
+	}
+
+	@Accessor("lastVertexBufferObject")
+	static void flywheel$setLastVBO(int id) {
+		throw new AssertionError();
+	}
+
+	@Accessor("lastIndexBufferObject")
+	static void flywheel$setLastEBO(int id) {
 		throw new AssertionError();
 	}
 }
