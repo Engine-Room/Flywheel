d1ee71de4e927e8e523326492cf4c61f93df1b1d
Jozufozu <jozsefaug@gmail.com>
2022-01-07 11:50:01 -0800

    Reduce merge conflicts with shader-sanity

--- src/main/java/com/jozufozu/flywheel/backend/source/Resolver.java
+++ src/main/java/com/jozufozu/flywheel/backend/source/Resolver.java
@@ -28,7 +28,7 @@ public class Resolver {
 	/**
 	 * Try and resolve all referenced source files, printing errors if any aren't found.
 	 */
-	public void resolve(SourceHolder sources) {
+	public void resolve(SourceFinder sources) {
 		for (FileResolution resolution : resolutions.values()) {
 			resolution.resolve(sources);
 		}
