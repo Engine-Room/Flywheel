23dfdece75d047879b17d6690708f3883a1b3e88
Aeiou <3160746+AeiouEnigma@users.noreply.github.com>
2022-01-10 01:38:30 -0500

    Add MixinPlugin to use a different version of LevelRendererMixin when sodium is installed

---
+++ src/main/java/com/jozufozu/flywheel/mixin/FlywheelMixinPlugin.java
@@ -0,0 +1,51 @@
+package com.jozufozu.flywheel.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class FlywheelMixinPlugin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		if (mixinClassName.contains(".compat.sodium.") && !FabricLoader.getInstance().isModLoaded("sodium"))
+			return false;
+		if (mixinClassName.contains(".flywheel.mixin.LevelRendererMixin") && FabricLoader.getInstance().isModLoaded("sodium"))
+			return false;
+		return true;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
