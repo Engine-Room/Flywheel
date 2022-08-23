6bb7ed8dc4daa308ae68f3a9ee500e6b182ccb13
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2022-08-22 20:00:21 -0700

    Add batching stage support

--- src/main/java/com/jozufozu/flywheel/mixin/RenderTypeMixin.java
+++ src/main/java/com/jozufozu/flywheel/mixin/RenderTypeMixin.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
-import com.jozufozu.flywheel.backend.instancing.batching.DrawBuffer;
+import com.jozufozu.flywheel.backend.instancing.batching.DrawBufferSet;
 import com.jozufozu.flywheel.extension.RenderTypeExtension;
 
 import net.minecraft.client.renderer.RenderType;
@@ -12,14 +12,14 @@ import net.minecraft.client.renderer.RenderType;
 @Mixin(RenderType.class)
 public class RenderTypeMixin implements RenderTypeExtension {
 	@Unique
-	private DrawBuffer flywheel$drawBuffer;
+	private DrawBufferSet flywheel$drawBufferSet;
 
 	@Override
 	@NotNull
-	public DrawBuffer flywheel$getDrawBuffer() {
-		if (flywheel$drawBuffer == null) {
-			flywheel$drawBuffer = new DrawBuffer(((RenderType) (Object) this).format());
+	public DrawBufferSet flywheel$getDrawBufferSet() {
+		if (flywheel$drawBufferSet == null) {
+			flywheel$drawBufferSet = new DrawBufferSet(((RenderType) (Object) this).format());
 		}
-		return flywheel$drawBuffer;
+		return flywheel$drawBufferSet;
 	}
 }
