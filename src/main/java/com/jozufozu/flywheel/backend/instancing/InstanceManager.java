6b9b2a9927175f8625a9d6efd848758f9e64f56f
Jozufozu <jozsefaug@gmail.com>
2023-03-29 14:11:29 -0700

    So many draws
    - Sort draws in an IndirectDrawSet by RenderStage and Material
    - Loop through ahead of time and determine what individual draws can be
      grouped together into a MultiDraw
      - Contiguous segments with the same RenderStage and Material can be
        merged into one MultiDraw call
    - Add material ID lookup for sorting purposes

--- src/main/java/com/jozufozu/flywheel/backend/instancing/InstanceManager.java
+++ src/main/java/com/jozufozu/flywheel/backend/instancing/InstanceManager.java
@@ -2,7 +2,6 @@ package com.jozufozu.flywheel.backend.instancing;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
