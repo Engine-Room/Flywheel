a526a48d235cd94620257f14f20208fba56c643a
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2023-03-29 18:32:08 -0700

    Minor mixin organization
    - Move LevelRendererInstanceUpdateMixin to instancemanage package
    - Ensure flywheel$ prefix is added to all mixin injector methods

--- src/main/java/com/jozufozu/flywheel/mixin/instancemanage/InstanceUpdateMixin.java
+++ src/main/java/com/jozufozu/flywheel/mixin/instancemanage/InstanceUpdateMixin.java
@@ -1,4 +1,4 @@
-package com.jozufozu.flywheel.mixin;
+package com.jozufozu.flywheel.mixin.instancemanage;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,7 +16,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(LevelRenderer.class)
-public class LevelRendererInstanceUpdateMixin {
+public class InstanceUpdateMixin {
 	@Shadow
 	private ClientLevel level;
 
