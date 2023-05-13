d3ca86e1185f786208c393ed126fe020ec99228d
Jozufozu <jozsefaug@gmail.com>
2023-05-13 12:01:12 -0700

    Actually do it somewhere else
    - Move main thread checking to TaskExecutor

--- src/main/java/com/jozufozu/flywheel/lib/task/OnMainThreadPlan.java
+++ src/main/java/com/jozufozu/flywheel/lib/task/OnMainThreadPlan.java
@@ -2,7 +2,6 @@ package com.jozufozu.flywheel.lib.task;
 
 import com.jozufozu.flywheel.api.task.Plan;
 import com.jozufozu.flywheel.api.task.TaskExecutor;
-import com.mojang.blaze3d.systems.RenderSystem;
 
 public record OnMainThreadPlan(Runnable task) implements ContextAgnosticPlan {
 	public static <C> Plan<C> of(Runnable task) {
@@ -11,12 +10,6 @@ public record OnMainThreadPlan(Runnable task) implements ContextAgnosticPlan {
 
 	@Override
 	public void execute(TaskExecutor taskExecutor, Runnable onCompletion) {
-		if (RenderSystem.isOnRenderThread()) {
-			task.run();
-			onCompletion.run();
-			return;
-		}
-
 		taskExecutor.scheduleForMainThread(() -> {
 			task.run();
 			onCompletion.run();
