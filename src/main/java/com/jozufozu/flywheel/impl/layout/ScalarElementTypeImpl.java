6b27614341ef3057fe6de27a38601f3ecb033e75
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2024-01-04 15:19:21 -0800

    Make layout map actually immutable
    - Also implement hashCode and equals on *ElementTypeImpl, Layout, and
    Layout.Element

--- src/main/java/com/jozufozu/flywheel/impl/layout/ScalarElementTypeImpl.java
+++ src/main/java/com/jozufozu/flywheel/impl/layout/ScalarElementTypeImpl.java
@@ -21,4 +21,27 @@ final class ScalarElementTypeImpl implements ScalarElementType {
 	public int byteSize() {
 		return byteSize;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + repr.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ScalarElementTypeImpl other = (ScalarElementTypeImpl) obj;
+		return repr == other.repr;
+	}
 }
