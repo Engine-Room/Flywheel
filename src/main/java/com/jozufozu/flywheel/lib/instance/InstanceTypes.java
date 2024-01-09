89f0e547173f9c8d893c86bd0011eacf20b8aa13
Jozufozu <jozsefaug@gmail.com>
2024-01-08 16:50:22 -0800

    Write me
    - Convert InstanceWriters into lambdas passed directly to the builder.
    - It's very nice to have everything in one place like that, will make
      tweaking the layout much easier.
    - Also micro-optimize the cull shader for TransformedInstance.

--- src/main/java/com/jozufozu/flywheel/lib/instance/InstanceTypes.java
+++ src/main/java/com/jozufozu/flywheel/lib/instance/InstanceTypes.java
@@ -1,12 +1,14 @@
 package com.jozufozu.flywheel.lib.instance;
 
 import org.jetbrains.annotations.ApiStatus;
+import org.lwjgl.system.MemoryUtil;
 
 import com.jozufozu.flywheel.Flywheel;
 import com.jozufozu.flywheel.api.instance.InstanceType;
 import com.jozufozu.flywheel.api.layout.FloatRepr;
 import com.jozufozu.flywheel.api.layout.IntegerRepr;
 import com.jozufozu.flywheel.api.layout.LayoutBuilder;
+import com.jozufozu.flywheel.lib.math.MatrixMath;
 
 public final class InstanceTypes {
 	public static final InstanceType<TransformedInstance> TRANSFORMED = SimpleInstanceType.builder(TransformedInstance::new)
@@ -16,7 +18,16 @@ public final class InstanceTypes {
 					.matrix("pose", FloatRepr.FLOAT, 4)
 					.matrix("normal", FloatRepr.FLOAT, 3)
 					.build())
-			.writer(TransformedWriter.INSTANCE)
+			.writer((ptr, instance) -> {
+				MemoryUtil.memPutShort(ptr, instance.blockLight);
+				MemoryUtil.memPutShort(ptr + 2, instance.skyLight);
+				MemoryUtil.memPutByte(ptr + 4, instance.r);
+				MemoryUtil.memPutByte(ptr + 5, instance.g);
+				MemoryUtil.memPutByte(ptr + 6, instance.b);
+				MemoryUtil.memPutByte(ptr + 7, instance.a);
+				MatrixMath.writeUnsafe(instance.model, ptr + 8);
+				MatrixMath.writeUnsafe(instance.normal, ptr + 72);
+			})
 			.vertexShader(Flywheel.rl("instance/transformed.vert"))
 			.cullShader(Flywheel.rl("instance/cull/transformed.glsl"))
 			.register();
@@ -29,7 +40,24 @@ public final class InstanceTypes {
 					.vector("pivot", FloatRepr.FLOAT, 3)
 					.vector("rotation", FloatRepr.FLOAT, 4)
 					.build())
-			.writer(OrientedWriter.INSTANCE)
+			.writer((ptr, instance) -> {
+				MemoryUtil.memPutShort(ptr, instance.blockLight);
+				MemoryUtil.memPutShort(ptr + 2, instance.skyLight);
+				MemoryUtil.memPutByte(ptr + 4, instance.r);
+				MemoryUtil.memPutByte(ptr + 5, instance.g);
+				MemoryUtil.memPutByte(ptr + 6, instance.b);
+				MemoryUtil.memPutByte(ptr + 7, instance.a);
+				MemoryUtil.memPutFloat(ptr + 8, instance.posX);
+				MemoryUtil.memPutFloat(ptr + 12, instance.posY);
+				MemoryUtil.memPutFloat(ptr + 16, instance.posZ);
+				MemoryUtil.memPutFloat(ptr + 20, instance.pivotX);
+				MemoryUtil.memPutFloat(ptr + 24, instance.pivotY);
+				MemoryUtil.memPutFloat(ptr + 28, instance.pivotZ);
+				MemoryUtil.memPutFloat(ptr + 32, instance.rotation.x);
+				MemoryUtil.memPutFloat(ptr + 36, instance.rotation.y);
+				MemoryUtil.memPutFloat(ptr + 40, instance.rotation.z);
+				MemoryUtil.memPutFloat(ptr + 44, instance.rotation.w);
+			})
 			.vertexShader(Flywheel.rl("instance/oriented.vert"))
 			.cullShader(Flywheel.rl("instance/cull/oriented.glsl"))
 			.register();
