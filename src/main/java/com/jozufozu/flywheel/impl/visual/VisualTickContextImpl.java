94a9146abcc9bd97dafc08840eba9859c6e8b3f2
Jozufozu <jozsefaug@gmail.com>
2024-01-28 20:58:07 -0800

    What makes it tick
    - Tick visual managers on post LevelTickEvent.
    - Remove all field from VisualTickContext, but keep the interface there
      for later review.
    - Remove tick update limiter, though apparently we were passing the
      frame limiter to the tick context :lwe:

--- src/main/java/com/jozufozu/flywheel/impl/visual/VisualTickContextImpl.java
+++ src/main/java/com/jozufozu/flywheel/impl/visual/VisualTickContextImpl.java
@@ -1,8 +1,6 @@
 package com.jozufozu.flywheel.impl.visual;
 
-import com.jozufozu.flywheel.api.visual.DistanceUpdateLimiter;
 import com.jozufozu.flywheel.api.visual.VisualTickContext;
 
-public record VisualTickContextImpl(double cameraX, double cameraY, double cameraZ,
-									DistanceUpdateLimiter limiter) implements VisualTickContext {
+public record VisualTickContextImpl() implements VisualTickContext {
 }
