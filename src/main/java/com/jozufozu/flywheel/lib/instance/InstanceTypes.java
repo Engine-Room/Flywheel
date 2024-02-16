f912be4834a7ad5da361774852652a123bf98e41
Jozufozu <jozsefaug@gmail.com>
2024-02-16 13:34:34 -0800

    NEA: Not Enough Attributes
    - Transformed had snuck up to attribute location 17
    - Combine overlay and light attributes into one
    - Flip order of overlay and light in instances to be consistent with
      the internal vertex layout

--- src/main/java/com/jozufozu/flywheel/lib/instance/InstanceTypes.java
+++ src/main/java/com/jozufozu/flywheel/lib/instance/InstanceTypes.java
@@ -14,8 +14,7 @@ public final class InstanceTypes {
 	public static final InstanceType<TransformedInstance> TRANSFORMED = SimpleInstanceType.builder(TransformedInstance::new)
 			.layout(LayoutBuilder.create()
 					.vector("color", FloatRepr.NORMALIZED_UNSIGNED_BYTE, 4)
-					.vector("light", IntegerRepr.SHORT, 2)
-					.vector("overlay", IntegerRepr.SHORT, 2)
+					.vector("overlay_light", IntegerRepr.SHORT, 4)
 					.matrix("pose", FloatRepr.FLOAT, 4)
 					.matrix("normal", FloatRepr.FLOAT, 3)
 					.build())
@@ -24,9 +23,9 @@ public final class InstanceTypes {
 				MemoryUtil.memPutByte(ptr + 1, instance.g);
 				MemoryUtil.memPutByte(ptr + 2, instance.b);
 				MemoryUtil.memPutByte(ptr + 3, instance.a);
-				MemoryUtil.memPutShort(ptr + 4, instance.blockLight);
-				MemoryUtil.memPutShort(ptr + 6, instance.skyLight);
-				MemoryUtil.memPutInt(ptr + 8, instance.overlay);
+				MemoryUtil.memPutInt(ptr + 4, instance.overlay);
+				MemoryUtil.memPutShort(ptr + 8, instance.blockLight);
+				MemoryUtil.memPutShort(ptr + 10, instance.skyLight);
 				MatrixMath.writeUnsafe(instance.model, ptr + 12);
 				MatrixMath.writeUnsafe(instance.normal, ptr + 76);
 			})
@@ -37,8 +36,7 @@ public final class InstanceTypes {
 	public static final InstanceType<OrientedInstance> ORIENTED = SimpleInstanceType.builder(OrientedInstance::new)
 			.layout(LayoutBuilder.create()
 					.vector("color", FloatRepr.NORMALIZED_UNSIGNED_BYTE, 4)
-					.vector("light", IntegerRepr.SHORT, 2)
-					.vector("overlay", IntegerRepr.SHORT, 2)
+					.vector("overlay_light", IntegerRepr.SHORT, 4)
 					.vector("position", FloatRepr.FLOAT, 3)
 					.vector("pivot", FloatRepr.FLOAT, 3)
 					.vector("rotation", FloatRepr.FLOAT, 4)
@@ -48,9 +46,9 @@ public final class InstanceTypes {
 				MemoryUtil.memPutByte(ptr + 1, instance.g);
 				MemoryUtil.memPutByte(ptr + 2, instance.b);
 				MemoryUtil.memPutByte(ptr + 3, instance.a);
-				MemoryUtil.memPutShort(ptr + 4, instance.blockLight);
-				MemoryUtil.memPutShort(ptr + 6, instance.skyLight);
-				MemoryUtil.memPutInt(ptr + 8, instance.overlay);
+				MemoryUtil.memPutInt(ptr + 4, instance.overlay);
+				MemoryUtil.memPutShort(ptr + 8, instance.blockLight);
+				MemoryUtil.memPutShort(ptr + 10, instance.skyLight);
 				MemoryUtil.memPutFloat(ptr + 12, instance.posX);
 				MemoryUtil.memPutFloat(ptr + 16, instance.posY);
 				MemoryUtil.memPutFloat(ptr + 20, instance.posZ);
