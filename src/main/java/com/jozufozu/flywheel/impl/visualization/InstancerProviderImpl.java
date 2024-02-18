9a5bd0cac8b3edb49aa6b56970ed1d6497d34e93
Jozufozu <jozsefaug@gmail.com>
2024-02-18 14:05:24 -0600

    Revert "Apply directly to the engine!"
    This reverts commit f3a88c25d5e8b9c295e8a4fcbd08c952bf2a60c0.

--- src/main/java/com/jozufozu/flywheel/impl/visualization/InstancerProviderImpl.java
+++ src/main/java/com/jozufozu/flywheel/impl/visualization/InstancerProviderImpl.java
@@ -2,7 +2,6 @@ package com.jozufozu.flywheel.impl.visualization;
 
 import java.util.function.Supplier;
 
-import com.jozufozu.flywheel.api.backend.DirectInstancerProvider;
 import com.jozufozu.flywheel.api.backend.Engine;
 import com.jozufozu.flywheel.api.event.RenderStage;
 import com.jozufozu.flywheel.api.instance.Instance;
@@ -18,11 +17,6 @@ public record InstancerProviderImpl(Engine engine,
 	@Override
 	public <I extends Instance> Instancer<I> instancer(InstanceType<I> type, Model model) {
 		return engine.instancer(type, Contexts.DEFAULT, model, renderStage);
-	}
-
-	@Override
-	public DirectInstancerProvider _directProvider() {
-		return engine;
 	}
 
 	@Override
