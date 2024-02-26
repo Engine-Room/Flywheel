73aaa4fde4a7f01a657255453c8fbeb8da5a2caf
Julian <julian@aternos.org>
2024-02-26 19:13:11 +0100

    Update to 1.20.4

--- src/main/java/com/jozufozu/flywheel/core/virtual/VirtualRenderWorld.java
+++ src/main/java/com/jozufozu/flywheel/core/virtual/VirtualRenderWorld.java
@@ -21,6 +21,7 @@ import net.minecraft.core.SectionPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.TickRateManager;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.flag.FeatureFlagSet;
@@ -324,6 +325,11 @@ public class VirtualRenderWorld extends Level implements FlywheelWorld {
 	@Override
 	public FeatureFlagSet enabledFeatures() {
 		return level.enabledFeatures();
+	}
+
+	@Override
+	public TickRateManager tickRateManager() {
+		return level.tickRateManager();
 	}
 
 	// ADDITIONAL OVERRRIDES
