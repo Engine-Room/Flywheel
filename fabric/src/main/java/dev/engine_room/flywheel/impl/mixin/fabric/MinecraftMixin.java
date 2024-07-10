10d032c53ecef73bc0b516ab375b2ff00b1e24f9
IThundxr <me@ithundxr.dev>
2024-07-10 13:03:07 -0400

    1.21 Port IV

--- fabric/src/main/java/dev/engine_room/flywheel/impl/mixin/fabric/MinecraftMixin.java
+++ fabric/src/main/java/dev/engine_room/flywheel/impl/mixin/fabric/MinecraftMixin.java
@@ -15,7 +15,7 @@ abstract class MinecraftMixin {
 	@Shadow
 	public ClientLevel level;
 
-	@Inject(method = "setLevel(Lnet/minecraft/client/multiplayer/ClientLevel;)V", at = @At("HEAD"))
+	@Inject(method = "setLevel(Lnet/minecraft/client/multiplayer/ClientLevel;Lnet/minecraft/client/gui/screens/ReceivingLevelScreen$Reason;)V", at = @At("HEAD"))
 	private void flywheel$onSetLevel(CallbackInfo ci) {
 		if (level != null) {
 			LevelAttached.invalidateLevel(level);
