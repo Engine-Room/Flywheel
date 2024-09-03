2537584a22dab9d25b6396ce38c96d3a6d12fa82
Jozufozu <jozsefaug@gmail.com>
2024-09-03 11:23:21 -0500

    The hardest problem
    - Rename most InstancePager terminology
    - Rename MODEL_INDEX buffer stuffs

--- common/src/backend/java/dev/engine_room/flywheel/backend/engine/indirect/BufferBindings.java
+++ common/src/backend/java/dev/engine_room/flywheel/backend/engine/indirect/BufferBindings.java
@@ -3,7 +3,7 @@ package dev.engine_room.flywheel.backend.engine.indirect;
 public final class BufferBindings {
 	public static final int INSTANCE = 0;
 	public static final int TARGET = 1;
-	public static final int MODEL_INDEX = 2;
+	public static final int PAGE_FRAME_DESCRIPTOR = 2;
 	public static final int MODEL = 3;
 	public static final int DRAW = 4;
 	public static final int LIGHT_LUT = 5;
