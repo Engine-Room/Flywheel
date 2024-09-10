fb41248c4cf12a48f95ce88408d1ceedba73e959
Rhys⁣⁣⁣⁣⁣⁣⁣ <98863820+rhysdh540@users.noreply.github.com>
2024-09-09 22:11:05 -0400

    fix loom version getting (#254)
    * fix loom version getting
    very cursed but it works :)
    
    * remove commented out stuff in settings.gradle.kts, unnecessary plugins in buildSrc, and configure buildSrc to download sources/javadoc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,12 +11,6 @@ pluginManagement {
         maven("https://repo.spongepowered.org/repository/maven-public")
         maven("https://maven.parchmentmc.org")
     }
-
-    // FIXME: This doesn't do anything. The actual version is always the one defined in buildSrc/build.gradle.kts.
-//    plugins {
-//        val arch_loom_version: String by settings
-//        id("dev.architectury.loom") version arch_loom_version
-//    }
 }
 
 rootProject.name = "Flywheel"
