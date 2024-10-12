39274f341ea2818be0f00a6d2dd73e856677de5e
IThundxr <me@ithundxr.dev>
2024-10-12 17:33:37 -0400

    fix

--- common/src/main/java/dev/engine_room/flywheel/impl/compat/EmbeddiumCompat.java
+++ common/src/main/java/dev/engine_room/flywheel/impl/compat/EmbeddiumCompat.java
@@ -2,10 +2,12 @@ package dev.engine_room.flywheel.impl.compat;
 
 import org.embeddedt.embeddium.api.ChunkDataBuiltEvent;
 
+import dev.engine_room.flywheel.lib.visualization.VisualizationHelper;
+
 public class EmbeddiumCompat {
 	public static void init() {
 		ChunkDataBuiltEvent.BUS.addListener(event -> {
-			event.getDataBuilder().removeBlockEntitiesIf(InstancedRenderDispatcher::tryAddBlockEntity);
+			event.getDataBuilder().removeBlockEntitiesIf(VisualizationHelper::tryAddBlockEntity);
 		});
 	}
 }
