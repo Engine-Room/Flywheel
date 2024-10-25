03f94fe88c3ad44668db0132fc8acc536f556608
Jozufozu <jozsefaug@gmail.com>
2024-10-25 13:13:05 -0700

    Why wait?
    - Immediately audit on client tick

--- fabric/src/testMod/java/dev/engine_room/flywheel/FlywheelTestModClient.java
+++ fabric/src/testMod/java/dev/engine_room/flywheel/FlywheelTestModClient.java
@@ -1,33 +1,27 @@
 package dev.engine_room.flywheel;
 
-import net.fabricmc.loader.api.FabricLoader;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class FlywheelTestModClient implements ClientModInitializer {
 	private static final Logger LOGGER = LoggerFactory.getLogger("Flywheel Test Mod");
-
-	private int ticks = 0;
 
 	@Override
 	public void onInitializeClient() {
 		LOGGER.info("Starting Test Mod, on Env: {}", FabricLoader.getInstance().getEnvironmentType());
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			LOGGER.info("Tick Count: {}", ticks);
+			LOGGER.info("Running mixin audit");
+			MixinEnvironment.getCurrentEnvironment()
+					.audit();
 
-			if (++ticks == 50) {
-				LOGGER.info("Running mixin audit");
-				MixinEnvironment.getCurrentEnvironment().audit();
-
-				LOGGER.info("Ran mixin audit, stopping client.");
-				client.stop();
-			}
+			LOGGER.info("Ran mixin audit, stopping client.");
+			client.stop();
 		});
 	}
 }
--- forge/src/testMod/java/dev/engine_room/flywheel/FlywheelTestModClient.java
+++ forge/src/testMod/java/dev/engine_room/flywheel/FlywheelTestModClient.java
@@ -1,39 +1,31 @@
 package dev.engine_room.flywheel;
 
-import net.minecraftforge.common.MinecraftForge;
-
-import net.minecraftforge.fml.loading.FMLLoader;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod("flywheel_testmod")
 public class FlywheelTestModClient {
 	private static final Logger LOGGER = LoggerFactory.getLogger("Flywheel Test Mod");
-
-	private int ticks = 0;
 
 	public FlywheelTestModClient() {
 		LOGGER.info("Starting Test Mod, on Dist: {}", FMLLoader.getDist());
 
 		MinecraftForge.EVENT_BUS.addListener((TickEvent.ClientTickEvent e) -> {
 			if (e.phase == TickEvent.Phase.END) {
-				LOGGER.info("Tick Count: {}", ticks);
+				LOGGER.info("Running mixin audit");
+				MixinEnvironment.getCurrentEnvironment()
+						.audit();
 
-				if (++ticks == 50) {
-					LOGGER.info("Running mixin audit");
-					MixinEnvironment.getCurrentEnvironment().audit();
-
-					LOGGER.info("Ran mixin audit, stopping client.");
-					Minecraft.getInstance().stop();
-				}
+				LOGGER.info("Ran mixin audit, stopping client.");
+				Minecraft.getInstance()
+						.stop();
 			}
 		});
 	}
