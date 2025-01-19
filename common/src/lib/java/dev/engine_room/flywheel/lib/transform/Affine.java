809a0eb61647bf79e7cb3d6fc6d67a52dc68cf8c
Jozufozu <jozsefaug@gmail.com>
2025-01-19 11:25:21 -0600

    Get JOML'd
    - Add more rotate methods to Affine and Rotate
    - Add optimized implementations in TransformedInstance

--- common/src/lib/java/dev/engine_room/flywheel/lib/transform/Affine.java
+++ common/src/lib/java/dev/engine_room/flywheel/lib/transform/Affine.java
@@ -7,6 +7,7 @@ import org.joml.Vector3fc;
 import com.mojang.math.Axis;
 
 import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
 
 public interface Affine<Self extends Affine<Self>> extends Translate<Self>, Rotate<Self>, Scale<Self> {
 	default Self rotateAround(Quaternionfc quaternion, float x, float y, float z) {
@@ -15,19 +16,18 @@ public interface Affine<Self extends Affine<Self>> extends Translate<Self>, Rota
 	}
 
 	default Self rotateAround(Quaternionfc quaternion, Vector3fc vec) {
-		return translate(vec.x(), vec.y(), vec.z()).rotate(quaternion)
-				.translateBack(vec.x(), vec.y(), vec.z());
+		return rotateAround(quaternion, vec.x(), vec.y(), vec.z());
 	}
 
 	default Self rotateCentered(Quaternionfc q) {
 		return rotateAround(q, CENTER, CENTER, CENTER);
 	}
 
-	default Self rotateCentered(float radians, Vector3fc axis) {
+	default Self rotateCentered(float radians, float axisX, float axisY, float axisZ) {
 		if (radians == 0) {
 			return self();
 		}
-		return rotateCentered(new Quaternionf().setAngleAxis(radians, axis.x(), axis.y(), axis.z()));
+		return rotateCentered(new Quaternionf().setAngleAxis(radians, axisX, axisY, axisZ));
 	}
 
 	default Self rotateCentered(float radians, Axis axis) {
@@ -37,10 +37,59 @@ public interface Affine<Self extends Affine<Self>> extends Translate<Self>, Rota
 		return rotateCentered(axis.rotation(radians));
 	}
 
+	default Self rotateCentered(float radians, Vector3fc axis) {
+		return rotateCentered(radians, axis.x(), axis.y(), axis.z());
+	}
+
+	default Self rotateCentered(float radians, Direction.Axis axis) {
+		return rotateCentered(radians, Direction.fromAxisAndDirection(axis, Direction.AxisDirection.POSITIVE));
+	}
+
 	default Self rotateCentered(float radians, Direction axis) {
-		if (radians == 0) {
-			return self();
-		}
-		return rotateCentered(radians, axis.step());
+		return rotateCentered(radians, axis.getStepX(), axis.getStepY(), axis.getStepZ());
+	}
+
+	default Self rotateCenteredDegrees(float degrees, float axisX, float axisY, float axisZ) {
+		return rotateCentered(Mth.DEG_TO_RAD * degrees, axisX, axisY, axisZ);
+	}
+
+	default Self rotateCenteredDegrees(float degrees, Axis axis) {
+		return rotateCentered(Mth.DEG_TO_RAD * degrees, axis);
+	}
+
+	default Self rotateCenteredDegrees(float degrees, Vector3fc axis) {
+		return rotateCentered(Mth.DEG_TO_RAD * degrees, axis);
+	}
+
+	default Self rotateCenteredDegrees(float degrees, Direction axis) {
+		return rotateCentered(Mth.DEG_TO_RAD * degrees, axis);
+	}
+
+	default Self rotateCenteredDegrees(float degrees, Direction.Axis axis) {
+		return rotateCentered(Mth.DEG_TO_RAD * degrees, axis);
+	}
+
+	default Self rotateXCentered(float radians) {
+		return rotateCentered(radians, Axis.XP);
+	}
+
+	default Self rotateYCentered(float radians) {
+		return rotateCentered(radians, Axis.YP);
+	}
+
+	default Self rotateZCentered(float radians) {
+		return rotateCentered(radians, Axis.ZP);
+	}
+
+	default Self rotateXCenteredDegrees(float degrees) {
+		return rotateXCentered(Mth.DEG_TO_RAD * degrees);
+	}
+
+	default Self rotateYCenteredDegrees(float degrees) {
+		return rotateYCentered(Mth.DEG_TO_RAD * degrees);
+	}
+
+	default Self rotateZCenteredDegrees(float degrees) {
+		return rotateZCentered(Mth.DEG_TO_RAD * degrees);
 	}
 }
