7ec8a870bddbde3b9e5e15cb0348e0cef080fa51
Jozufozu <jozsefaug@gmail.com>
2025-02-23 13:53:22 -0800

    The choice is yours
    - Add ORDER_INDEPENDENT transparency enum
    - Document all transparency modes
    - Use ORDER_INDEPENDENT for the default translucent chunk materials

--- common/src/api/java/dev/engine_room/flywheel/api/material/Transparency.java
+++ common/src/api/java/dev/engine_room/flywheel/api/material/Transparency.java
@@ -1,10 +1,81 @@
 package dev.engine_room.flywheel.api.material;
 
 public enum Transparency {
+	/**
+	 * No blending. Used for solid and cutout geometry.
+	 */
 	OPAQUE,
+
+	/**
+	 * Additive blending.
+	 *
+	 * <p>Each fragment blends color and alpha with the following equation:
+	 * <pre>
+	 * {@code
+	 * out = src + dst
+	 * }
+	 * </pre>
+	 */
 	ADDITIVE,
+
+	/**
+	 * Lightning transparency.
+	 *
+	 * <p>Each fragment blends color and alpha with the following equation:
+	 * <pre>
+	 * {@code
+	 * out = src * alpha_src + dst
+	 * }
+	 * </pre>
+	 */
 	LIGHTNING,
+
+	/**
+	 * Glint transparency. Used for the enchantment effect.
+	 *
+	 * <p>Each fragment blends with the following equations:
+	 * <pre>
+	 * {@code
+	 * color_out = color_src^2 + color_dst
+	 * alpha_out = alpha_dst
+	 * }
+	 * </pre>
+	 */
 	GLINT,
+
+	/**
+	 * Crumbling transparency. Used for the block breaking overlay.
+	 *
+	 * <p>Each fragment blends with the following equations:
+	 * <pre>
+	 * {@code
+	 * color_out = 2 * color_src * color_dst
+	 * alpha_out = alpha_src
+	 * }
+	 * </pre>
+	 */
 	CRUMBLING,
+
+	/**
+	 * Translucent transparency.
+	 *
+	 * <p>Each fragment blends with the following equations:
+	 * <pre>
+	 * {@code
+	 * color_out = color_src * alpha_src + color_dst * (1 - alpha_src)
+	 * alpha_out = alpha_src + alpha_dst * (1 - alpha_src)
+	 * }
+	 * </pre>
+	 */
 	TRANSLUCENT,
+
+	/**
+	 * If supported by the backend, this mode will use OIT that approximates {@code TRANSLUCENT} transparency.
+	 *
+	 * <p>If a backend does not support OIT, it must treat this the same as {@code TRANSLUCENT}.
+	 *
+	 * <p>It is recommended to use this option when possible, though for cases where blend modes are used as an
+	 * overlay against solid geometry the order dependent modes are preferred.
+	 */
+	ORDER_INDEPENDENT,
 }
--- common/src/backend/java/dev/engine_room/flywheel/backend/engine/MaterialRenderState.java
+++ common/src/backend/java/dev/engine_room/flywheel/backend/engine/MaterialRenderState.java
@@ -135,7 +135,7 @@ public final class MaterialRenderState {
 			RenderSystem.enableBlend();
 			RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.DST_COLOR, GlStateManager.DestFactor.SRC_COLOR, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
 		}
-		case TRANSLUCENT -> {
+		case TRANSLUCENT, ORDER_INDEPENDENT -> {
 			RenderSystem.enableBlend();
 			RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
 		}
