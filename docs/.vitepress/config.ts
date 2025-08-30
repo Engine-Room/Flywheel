708a45d5b911653735ef41fa7e8a662ea85a88f2
IThundxr <me@ithundxr.dev>
2025-08-30 15:18:06 -0400

    Groundwork for docs (#284)

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,50 @@
+// noinspection JSUnusedGlobalSymbols
+
+import { defineConfig } from "vitepress";
+import users from "./sidebars/users";
+import developers from "./sidebars/developers";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Flywheel Docs",
+  description: "A modern engine for modded Minecraft.",
+
+  cleanUrls: true,
+  lastUpdated: true,
+
+  head: [["link", { rel: "icon", href: "/flywheel-icon-small.webp" }]],
+
+  themeConfig: {
+    logo: {
+      src: "/flywheel-icon-small.webp",
+      width: 24,
+      height: 24,
+    },
+
+    search: {
+      // TODO - Switch to Algolia Search post deployment
+      provider: "local",
+    },
+
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [{ text: "Home", link: "/" }],
+
+    sidebar: {
+      ...users,
+      ...developers,
+    },
+
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/Engine-Room/Flywheel",
+      },
+      { icon: "discord", link: "https://discord.gg/ambsHEyaAD" },
+    ],
+
+    editLink: {
+      pattern: "https://github.com/Engine-Room/Flywheel/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+  },
+});
---
+++ docs/.vitepress/sidebars/developers.ts
@@ -0,0 +1,7 @@
+import {DefaultTheme} from "vitepress";
+
+export default {
+    "/developers": [
+        {}
+    ],
+} as DefaultTheme.SidebarMulti;
