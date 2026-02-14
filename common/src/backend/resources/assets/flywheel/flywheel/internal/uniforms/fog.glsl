5618a0bb96016a11065e0cb5784c6b52fc3993f8
IThundxr <me@ithundxr.dev>
2026-02-14 12:28:51 -0500

    Fog fixes

--- common/src/backend/resources/assets/flywheel/flywheel/internal/uniforms/fog.glsl
+++ common/src/backend/resources/assets/flywheel/flywheel/internal/uniforms/fog.glsl
@@ -1,7 +1,9 @@
 layout(std140) uniform _FlwFogUniforms {
     vec4 flw_fogColor;
-    vec2 flw_fogEnvironmentalRange;
-    vec2 flw_fogRenderDistanceRange;
+    float flw_fogEnvironmentalStart;
+    float flw_fogEnvironmentalEnd;
+    float flw_fogRenderDistanceStart;
+    float flw_fogRenderDistanceEnd;
     float flw_fogSkyEnd;
     float flw_fogCloudEnd;
 };
