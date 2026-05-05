64889d560e0221b5a8f8384cab10cd0fbaa0a60f
IThundxr <me@ithundxr.dev>
2026-05-05 17:07:28 -0400

    Improve per part AO mixin

--- neoforge/src/main/java/dev/engine_room/flywheel/impl/mixin/neoforge/ModelBlockRendererMixin.java
+++ neoforge/src/main/java/dev/engine_room/flywheel/impl/mixin/neoforge/ModelBlockRendererMixin.java
@@ -1,37 +1,35 @@
 package dev.engine_room.flywheel.impl.mixin.neoforge;
 
-import java.util.List;
-
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import dev.engine_room.flywheel.lib.model.baked.BlockStateModelBufferer;
 import dev.engine_room.flywheel.lib.model.baked.NeoForgeMeshEmitter;
 import dev.engine_room.flywheel.lib.model.baked.NeoForgeMeshEmitterManager;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
+import net.minecraft.client.renderer.block.BlockQuadOutput;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
-import net.minecraft.client.renderer.block.dispatch.BlockStateModelPart;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.resources.model.geometry.BakedQuad;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(ModelBlockRenderer.class)
 abstract class ModelBlockRendererMixin {
-	@Inject(method = "tesselateAmbientOcclusion", at = @At(value = "FIELD", target = "net/minecraft/client/renderer/block/ModelBlockRenderer.DIRECTIONS : [Lnet/minecraft/core/Direction;", opcode = Opcodes.GETSTATIC))
-	private void onTesselateWithAO(CallbackInfo ci, @Local(name = "part") BlockStateModelPart part, @Local(name = "ao") boolean ao) {
+	@WrapOperation(method = "tesselateAmbientOcclusion", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;putQuadWithTint(Lnet/minecraft/client/renderer/block/BlockQuadOutput;FFFLnet/minecraft/client/renderer/block/BlockAndTintGetter;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/client/resources/model/geometry/BakedQuad;)V"))
+	private void onTesselateWithAO(ModelBlockRenderer instance, BlockQuadOutput output, float x, float y, float z, BlockAndTintGetter level, BlockState state, BlockPos pos, BakedQuad quad, Operation<Void> original, @Local(name = "ao") boolean ao) {
 		ScopedValue<NeoForgeMeshEmitterManager> value = BlockStateModelBufferer.EMITTER_MANAGER;
 		if (value.isBound()) {
-			List<BakedQuad> quads = part.getQuads(null);
+			ChunkSectionLayer layer = quad.materialInfo().layer();
+			NeoForgeMeshEmitter meshEmitter = value.get().getEmitter(layer);
 
-			if (!quads.isEmpty()) {
-				ChunkSectionLayer layer = quads.getFirst().materialInfo().layer();
-				NeoForgeMeshEmitter meshEmitter = value.get().getEmitter(layer);
-
-				meshEmitter.prepareForPart(ao);
-			}
+			meshEmitter.prepareForPart(ao);
 		}
+
+		original.call(instance, output, x, y, z, level, state, pos, quad);
 	}
 }
